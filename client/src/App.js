122e8dada433c0c3f9044b7c1d17a1f9a6c5b422
epiadam <adam.epinger@outlook.com>
2025-11-02 01:45:37 +0100

    Save local changes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // Sets up routing and layout for the portfolio app.
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// Remove 'BrowserRouter as Router' from this import
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -10,19 +11,19 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/contacts" element={<ContactsPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    // Remove the <Router> wrapper from here
+    <div className="App">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+        </Routes>
+      </main>
+    </div>
+    // And here
   );
 }
 
